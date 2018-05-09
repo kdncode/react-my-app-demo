427eb39e177fa30031d4c24976586d1092e066b0
kdncode <kdncode@gmail.com>
2018-05-08 21:11:59 -0500

    added State

--- src/App.js
+++ src/App.js
@@ -1,50 +1,65 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
-import Product from './components/Product';
 
+/* STATE */
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            products : [
+                { 
+                    id: 1,
+                    name: 'iPhone X',
+                    price: '$1000',
+                    image: 'https://ss7.vzw.com/is/image/VerizonWireless/iphone-x-kf-device-tab-d-3-retina?$pngalpha$&scl=1',
+                    status: true
+                },
+                {
+                    id: 2,
+                    name: 'Samsung S9',
+                    price: '$900',
+                    image: 'https://ss7.vzw.com/is/image/VerizonWireless/pdp-samsung-star2-feature1-d-0218?$pngalpha$&scl=1',
+                    status: false
+                },
+                {
+                    id: 3,
+                    name: 'Pixel 2',
+                    price: '$700',
+                    image: 'https://boygeniusreport.files.wordpress.com/2017/10/google-pixel-2-xl.png?w=782',
+                    status: false
+                }
+            ],
 
-  OnClickMe() {
-      alert('hello')
-  }
+            isActive : true
+        };
+    }
   
-  render() {
-    var products = [
-        { 
-            id: 1,
-            name: 'iPhone X',
-            price: '$1000',
-            image: 'https://ss7.vzw.com/is/image/VerizonWireless/iphone-x-kf-device-tab-d-3-retina?$pngalpha$&scl=1',
-            status: true
-        },
-        {
-            id: 2,
-            name: 'Samsung S9',
-            price: '$900',
-            image: 'https://ss7.vzw.com/is/image/VerizonWireless/pdp-samsung-star2-feature1-d-0218?$pngalpha$&scl=1',
-            status: false
-        },
-        {
-            id: 3,
-            name: 'Pixel 2',
-            price: '$700',
-            image: 'https://boygeniusreport.files.wordpress.com/2017/10/google-pixel-2-xl.png?w=782',
-            status: false
-        }
-    ];
+  onSetState = () => {
+    // if( this.state.isActive === true ) {
+    //     this.setState( { isActive : false })
+    // } else {
+    //     this.setState( { isActive : true })
+    // }
 
-    let elements = products.map( (product, index) => {
+    // Or
+    this.setState( { isActive : !this.state.isActive })
+  }
+
+  render() {
+
+    let elements = this.state.products.map( (product, index) => {
         let results = '';
         if (product.status) {
-            return <Product 
-                        key = {product.id}
-                        name={product.name}
-                        price={product.price} 
-                        image={product.image}
-                    />
+            return  <tr key = {index} >
+                        <td>{index}</td>
+                        <td>{product.name}</td>
+                        <td>
+                            <span className="label label-success">{product.price}</span>
+                        </td>
+                    </tr>
         }
         return results;
-    })
+    });
 
     return (
       <div>
@@ -52,12 +67,21 @@ class App extends Component {
         <div className="container">
             <div className="row">
                 <div className="row">
+                    <table className="table table-stripe table-bordered table-hover">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            { elements }
+                        </tbody>
+                    </table>
                     <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                        { elements }
-                    </div>
-                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                        <button onClick={this.OnClickMe} type="button" className="btn btn-warning">
-                            Click me
+                        <button onClick={this.onSetState} type="button" className="btn btn-warning">
+                            Active : {this.state.isActive === true ? 'true' : 'false'}
                         </button>
                     </div>
                 </div>
@@ -67,5 +91,73 @@ class App extends Component {
     );
   }
 }
+
+
+
+// class App extends Component {
+
+//   OnClickMe() {
+//       alert('hello')
+//   }
+
+//   render() {
+//     var products = [
+//         { 
+//             id: 1,
+//             name: 'iPhone X',
+//             price: '$1000',
+//             image: 'https://ss7.vzw.com/is/image/VerizonWireless/iphone-x-kf-device-tab-d-3-retina?$pngalpha$&scl=1',
+//             status: true
+//         },
+//         {
+//             id: 2,
+//             name: 'Samsung S9',
+//             price: '$900',
+//             image: 'https://ss7.vzw.com/is/image/VerizonWireless/pdp-samsung-star2-feature1-d-0218?$pngalpha$&scl=1',
+//             status: false
+//         },
+//         {
+//             id: 3,
+//             name: 'Pixel 2',
+//             price: '$700',
+//             image: 'https://boygeniusreport.files.wordpress.com/2017/10/google-pixel-2-xl.png?w=782',
+//             status: false
+//         }
+//     ];
+
+//     let elements = products.map( (product, index) => {
+//         let results = '';
+//         if (product.status) {
+//             return <Product 
+//                         key = {product.id}
+//                         name={product.name}
+//                         price={product.price} 
+//                         image={product.image}
+//                     />
+//         }
+//         return results;
+//     })
+
+//     return (
+//       <div>
+//         <Header />
+//         <div className="container">
+//             <div className="row">
+//                 <div className="row">
+//                     <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+//                         { elements }
+//                     </div>
+//                     <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+//                         <button onClick={this.OnClickMe} type="button" className="btn btn-warning">
+//                             Click me
+//                         </button>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
